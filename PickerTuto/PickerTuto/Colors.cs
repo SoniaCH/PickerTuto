ce038e5351b9093b76f86830bc7d918db21d24f3
Sonia Chnini <s.chnini@root.sa>
2018-04-10 22:20:31 +0100

    this is an example for using the piker and how to manipulate the item selected. an example for ListView: create , add, delete also Grid

---
+++ PickerTuto/PickerTuto/Colors.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickerTuto
+{
+    public class Colors
+    {
+        public string NameEn { get; set; } 
+    }
+}
---
+++ PickerTuto/PickerTuto/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickerTuto
+{
+	public partial class MainPage : ContentPage
+	{
+        ObservableCollection<Colors> MyList = new ObservableCollection<Colors>();
+        ObservableCollection<Colors> MyListChosen = new ObservableCollection<Colors>();
+
+        public MainPage()
+		{
+			InitializeComponent();
+            BindingContext = this;
+            TheValue.ItemsSource = MyList;
+		}
+        public void AddBtn( object sender,EventArgs args)
+        {
+            var selectedValue = pikerDays.Items[pikerDays.SelectedIndex].ToString();
+            var colorChosen = new Colors
+            {
+                NameEn = selectedValue
+            };
+            
+            MyList.Add(colorChosen);
+            TheValue.ItemsSource = MyList;
+        }
+        public void ToDelete(object sender,EventArgs args)
+        {
+            var item = (Xamarin.Forms.Button)sender;
+            Colors listitem = (from itm in MyList
+                             where itm.NameEn == item.CommandParameter.ToString()
+                             select itm)
+                            .FirstOrDefault<Colors>();
+            MyList.Remove(listitem);
+        }
+    }
+}
